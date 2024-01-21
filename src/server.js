04a15074ebad459e49433974e0f9961c6ba9eaa9
Lucas Roseti <lc.roseti@gmail.com>
2024-01-20 21:16:16 -0300

    Stateful and Headers
    - Creating user
    - Listing users
    - Stateful concept
    - Headers with json

--- src/server.js
+++ src/server.js
@@ -1,13 +1,23 @@
 import http from 'node:http'
 
+const users = []
+
 const server = http.createServer((req, res) => {
 	const { method, url } = req
 
 	if (method === 'GET' && url === '/users') {
-		return res.end('Users list') //Early return
+		return res
+			.setHeader('Content-Type', 'application/json')	
+			.end(JSON.stringify(users)) //Early return
 	}
 
 	if (method === 'POST' && url === '/users') {
+		users.push({
+			id: 1,
+			name: 'John Doe',
+			email: 'johndoe@example.com'
+		})
+
 		return res.end('User create')
 	}
 
